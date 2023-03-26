dfe1bb6d299c41db16b2467a0c00854f9bae36df
Ethan Strominger <ethanstrominger2@gmail.com>
2023-03-26 19:34:55 -0400

    mobtimer-api adjust

--- scripts/start-frontend-watch.sh
+++ scripts/start-frontend-watch.sh
@@ -1,5 +1,5 @@
-./scripts/generate-mobtimer-api-exports-no-watch.sh
-./scripts/symlink-mobtimer-api-frontend.sh
+# ./scripts/generate-mobtimer-api-exports-no-watch.sh
+# ./scripts/symlink-mobtimer-api-frontend.sh
 # cd mobtimer-api
 # ./start-no-watch.sh
 sleep 2 # to make sure other components are compiled first
